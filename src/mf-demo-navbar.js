1935ef418aa0ca3b412f97f4d0da94995703c0f7
Michael Ellis <michael.ellis@vertexinc.com>
2020-11-17 11:33:52 -0500

    Adds shadow DOM

--- src/mf-demo-navbar.js
+++ src/mf-demo-navbar.js
@@ -1,13 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import singleSpaReact from "single-spa-react";
+import root from "react-shadow";
 import Root from "./root.component";
 import "./set-public-path";
+
+const styles = require("!!to-string-loader!css-loader!./styles.css");
+
+class WrappedRoot extends React.Component {
+  render() {
+    return (
+      <root.div mode="open">
+        <div className="mf-demo-navbar">
+          <style type="text/css">{styles}</style>
+          <Root />
+        </div>
+      </root.div>
+    );
+  }
+}
 
 const lifecycles = singleSpaReact({
   React,
   ReactDOM,
-  rootComponent: Root,
+  rootComponent: WrappedRoot,
 });
 
 export const { bootstrap, mount, unmount } = lifecycles;
--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,22 @@ const webpackMerge = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react");
 const path = require("path");
 
-module.exports = webpackConfigEnv => {
+module.exports = (webpackConfigEnv) => {
   const defaultConfig = singleSpaDefaults({
     orgName: "mf-demo",
     projectName: "navbar",
-    webpackConfigEnv
+    webpackConfigEnv,
   });
 
   return webpackMerge.smart(defaultConfig, {
     // customizations can go here
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
   });
 };
